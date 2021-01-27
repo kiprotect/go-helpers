ccfa111164c338c5ee8da29874c34cca30c3e4f7
Andreas Dewes <andreas.dewes@kiprotect.com>
2021-01-27 22:11:24 +0100

    Updated copyright texts; add context to subform validation.

--- forms/coerce.go
+++ forms/coerce.go
@@ -1,3 +1,17 @@
+// KIProtect Go-Helpers - Golang Utility Functions
+// Copyright (C) 2019-2021  KIProtect GmbH (HRB 208395B) - Germany
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the 3-Clause BSD License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// license for more details.
+//
+// You should have received a copy of the 3-Clause BSD License
+// along with this program.  If not, see <https://opensource.org/licenses/BSD-3-Clause>.
+
 package forms
 
 import (
